9831b11c88eb82d7e8b29d5c13a81641c97eaafc
SravaniMethukul <methukulasravani14@gmail.com>
2025-05-25 18:59:23 +0200

    Basic Authentication in window

---
+++ src/test/java/BasicAuthentication.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+import java.util.function.Predicate;
+
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+
+public class BasicAuthentication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		DevTools devTools = driver.getDevTools();
+		
+		devTools.createSession();
+		
+		//predicate
+		
+		Predicate<URI> uriPredicate = uri -> uri.getHost().contains("httpbin.org");
+		((HasAuthentication)driver).register(uriPredicate, UsernameAndPassword.of("foo", "bar"));
+		driver.get("http://httpbin.org/basic-auth/foo/bar");
+		
+	}
+
+}
